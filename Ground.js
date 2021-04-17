da44ec8f04a7bb9463918e73976353be8a25fa1e
arti-garg <75868830+arti-garg@users.noreply.github.com>
2021-04-17 21:24:43 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,25 @@
+class Ground {
+ constructor(x, y, w, h){
+ var options = {isStatic : true}
+
+ this.body = Bodies.rectangle(x, y, w, h, options);
+ World.add(world, this.body);
+
+this.w= w;
+this.h= h;
+
+ }
+
+ display(){
+
+    var pos = this.body.position;
+
+    fill ("white")
+    rectMode(CENTER);
+    rect(pos.x, pos.y, this.w, this.h)
+ }
+
+
+
+
+}
